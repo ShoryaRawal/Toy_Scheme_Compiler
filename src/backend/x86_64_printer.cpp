d7e3fb552a6c3cb7cb30b7d5725d05e47d3f89cf
ShoryaRawal <46308728+ShoryaRawal@users.noreply.github.com>
2026-05-31 16:23:49 +0530

    asm syntax patch

--- src/backend/x86_64_printer.cpp
+++ src/backend/x86_64_printer.cpp
@@ -4,9 +4,9 @@ namespace tscm{
 	X86_64Printer::X86_64Printer(std::ostream & output) : output_(output){}
 
 	void X86_64Printer::print(const AssemblyProgram & program){
-		output_ << "global main\n\n";
+		output_ << "global _start\n\n";
 		output_ << "section .text\n\n";
-		output_ << "main:\n";
+		output_ << "_start:\n";
 
 		for(const auto & instruction : program.instructions){
 			output_ << "	" << instruction.opcode;
@@ -19,6 +19,6 @@ namespace tscm{
 			}
 			output_ << "\n";
 		}
-		output_ << "	ret\n";
+		output_ << "	mov rdi, rax\n\tmov rax, 60\n\tsyscall";
 	}
 }
